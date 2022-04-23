0301799e61386456f9ccd91097b311db4e1361d1
Yegor <ultrahentaiturbolord@gmail.com>
2022-04-23 21:12:31 +0300

    Finished

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,39 @@ import './styles/index.scss';
 setTimeout(function () {
   document.body.className = '';
 }, 200);
+
+const AddListenerExpandOnClick = (className: string) => {
+  document.querySelectorAll(`.${className}`).forEach((element) => {
+    element.addEventListener('click', () => {
+      element.querySelector(`.${className}-content`)?.classList.add('expanded');
+    });
+    element.addEventListener('click', () => {
+      element.querySelector(`.${className}-content`)?.classList.add('expanded');
+    });
+  });
+};
+
+const classList = [
+  'header__top-item-dropdown',
+  'header__main-nav-item--dropdown',
+];
+
+classList.forEach((className) => AddListenerExpandOnClick(className));
+document.addEventListener('click', (event) => {
+  const target = event.target as HTMLElement;
+
+  document.querySelectorAll('.expanded').forEach((expandElement) => {
+    const element = expandElement as HTMLElement;
+    let isValid = false;
+    classList.forEach((className) => {
+      if (target.closest(`.${className}`)) isValid = true;
+    });
+    if (
+      !isValid ||
+      (isValid &&
+        !target.contains(element) &&
+        target.closest('.expanded') !== element)
+    )
+      element.classList.remove('expanded');
+  });
+});
